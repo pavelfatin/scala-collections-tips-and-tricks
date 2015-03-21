071aefbecc3acc7cccbe8bad0a277187449926ac
Pavel Fatin <mail@pavelfatin.com>
2015-03-21 16:51:08 +0100

    filterKeys and mapValues code updated

--- maps/maps.scala
+++ maps/maps.scala
@@ -40,7 +40,7 @@ map.values
 map.valuesIterator
 
 
-// Avoid using "filterKeys"
+// Be careful with "filterKeys"
 
 // Before
 map.filterKeys(p)
@@ -49,13 +49,43 @@ map.filterKeys(p)
 map.filter(p(_._1))
 
 
-// Avoid using "mapValues"
+// Unambiguous synonym
+
+type MapView[A, +B] = Map[A, B]
+
+implicit class MapExt[A, +B](val map: Map[A, B]) extends AnyVal {
+  def withKeyFilter(p: A => Boolean): MapView[A, B] =
+    map.filterKeys(p)
+}
+
+
+// Helper method
+
+def get(k: T) = if (p(k)) map.get(k) else None
+
+
+// Be careful with "mapValues"
 
 // Before
 map.mapValues(f)
 
 // After
 map.map(f(_._2))
+
+
+// Unambiguous synonym
+	
+type MapView[A, +B] = Map[A, B]
+
+implicit class MapExt[A, +B](val map: Map[A, B]) extends AnyVal {
+  def withValueMapper[C](f: B => C): MapView[A, C] =
+    map.mapValues(f)
+}
+
+
+// Helper method
+
+def get(k: T) = map.get(k).map(f)
 
 
 // Don't filter out keys manually
